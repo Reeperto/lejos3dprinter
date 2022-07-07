551c6c1eb3d066746978661097b10038416b64ee
Eli Griffiths <tooblecain@gmail.com>
2022-07-06 23:02:18 -0700

    Add Todo's and begin GCode interpreter

--- src/ev3dprinter/GCode.java
+++ src/ev3dprinter/GCode.java
@@ -2,4 +2,9 @@ package ev3dprinter;
 
 public class GCode {
 
+    public static String[] parseGCode(String gcode) {
+        String[] gcodeLines = gcode.split("\n");
+        return gcodeLines;
+    }
+
 }
